b93f25941b73d2c11a52035ba18f27de1d370441
Etienne Garnier <garnier.etienne@gmail.com>
2012-11-21 21:00:35 +0100

    split migration into two sql file: for testing and for real use (development or production)

--- migration-test.sql
+++ migration-test.sql
@@ -1,3 +1,11 @@
+/* Testing tables */
+CREATE TABLE cached_objects ("id" SERIAL PRIMARY KEY, "name" TEXT, "goal" TEXT);
+CREATE TABLE custom_classes ("id" SERIAL PRIMARY KEY, "name" TEXT, "goal" TEXT);
+CREATE TABLE another_classes ("id" SERIAL PRIMARY KEY, number INTEGER, "custom_class_id" INTEGER REFERENCES custom_classes("id"));
+CREATE TABLE mailbox_tests ("id" SERIAL PRIMARY KEY, "name" TEXT);
+CREATE TABLE message_tests ("id" SERIAL PRIMARY KEY, "subject" TEXT, "from" TEXT, "mailbox_test_id" INTEGER REFERENCES mailbox_tests("id"));
+
+/* Real tables */
 CREATE TABLE providers ("id" SERIAL PRIMARY KEY, "name" TEXT, "imap_host" TEXT, "imap_port" INTEGER, "imap_secure" BOOLEAN, "smtp_host" TEXT, "smtp_port" INTEGER, "smtp_secure" BOOLEAN);
 CREATE TABLE domains ("id" SERIAL PRIMARY KEY, "name" TEXT, "provider_id" INTEGER REFERENCES providers("id"));
 CREATE TABLE accounts ("id" SERIAL PRIMARY KEY, "email_address" TEXT, "provider_id" INTEGER REFERENCES providers("id"));
